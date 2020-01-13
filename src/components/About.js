9b5ff50c779f64bdf89ec57d43d17efbb8adbf0f
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-13 01:45:32 -0500

    font awesome update with minor update on about section

--- src/components/About.js
+++ src/components/About.js
@@ -15,6 +15,15 @@ const About = () => {
                 <Grid item sm={12} md={6}>
                     <div className='intro-container'>
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <div className='social-media-icons'>
+                            <i class="fab fa-github-square"></i>
+                            <i class="fab fa-behance-square"></i>
+                            <i class="fab fa-medium"></i>
+                            <i class="fab fa-dev"></i>
+                            <i class="fab fa-blogger"></i>
+                        </div>
+                        
+                        
                     </div>
                 </Grid>
             </Grid>
