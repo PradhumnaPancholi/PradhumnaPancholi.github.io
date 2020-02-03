6555dd43802912b6ee66df98a38234a74ece786a
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-03 18:56:07 -0500

    header responsiveness fix attempt one

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 const Header = () => {
     return(
-        <div className='header' width={1}>
-            <Card fluid className='card' width={1}>
-                <CardContent className='card-content'>
-                    <Typography>Pradhumna Pancholi</Typography>
-                    <Typography>Software Developer & Blogger @ Geekstuffpro.tech</Typography>
-                </CardContent>
-            </Card>
+        <div className='header'>
+                <Typography>Pradhumna Pancholi</Typography>
+                <Typography>Software Developer & Blogger @ Geekstuffpro.tech</Typography>
         </div>
     )
 }
