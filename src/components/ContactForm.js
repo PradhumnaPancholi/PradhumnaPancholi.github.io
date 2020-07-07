1321b460b25db5f6a63a35fa8299d479c0eeec29
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-07-07 16:08:13 -0400

    customized contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,42 @@
-import { TextField, Typography } from '@material-ui/core';
+import { Button, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 
-const ContactForm = () => {
+const ContactForm = (props) => {
 
-    const contactFormDetails = ['First Name', 'Last Name', 'Email', 'Message']
+    const contactFormDetails = ['First Name', 'Last Name', 'Email']
     return(
         <div className='contact-form'>
             <Typography className='section-header'>Get In Touch</Typography>
             <hr className='under-line' />
             {/* actual contact form */}
             <form autoComplete='on' defaultValue>
+                {/* input feilds */}
                 {contactFormDetails.map((item) => (
-                    <div>
-                        <TextField className='contact-form-field' id="filled-basic" label={item} variant="filled" />
+                    <div className='input'>
+                        <TextField 
+                            required
+                            className='contact-form-field' 
+                            id='filled-basic' 
+                            label={item} 
+                            variant='filled'
+                        />
                     </div>
                 ))}
+                {/* text area for message */}
+                <div className='input'>
+                    <TextField
+                        required
+                        multiline
+                        id='filled-basic'
+                        variant='filled'
+                        label='Message'
+                        className='contact-form-field'
+                        aria-label='message'
+                        rows={4}
+                    />
+                </div>
+                {/* send button */}
+                <Button>Send</Button>
             </form>
         </div>
     )
