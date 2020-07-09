ae7a137facaefb3eba407e0372143639e2fdc12c
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-07-09 18:40:36 -0400

    added project data and setup for rendering in project card.

--- src/components/Layout/ProjectCard.js
+++ src/components/Layout/ProjectCard.js
@@ -1,17 +1,16 @@
 import { Button, Card, CardContent, Link, Typography } from '@material-ui/core';
 import React from 'react';
 
-const ProjectCard = (data) => {
-    // const {name, link, description} = data
-    console.log(data)
+const ProjectCard = ({data}) => {
+    const {name, link, description} = data
     return(   
         <div>
             <Card className='project-card'>
                 <CardContent className='project-content'>
-                    <Typography variant='h5'>Project Name</Typography>
-                    <Typography variant='body2' component='p'>1Esse sunt esse minim officia sunt sit Lorem eu sint aliqua labore in.In qui id occaecat sunt Lorem consequat consectetur reprehenderit duis.Excepteur ut sint laborum elit velit nisi labore officia.</Typography>
+                    <Typography variant='h5'>{name}</Typography>
+                    <Typography variant='body2' component='p'>{description}</Typography>
                     <Button>
-                        <Link to=''>Check it out</Link>
+                        <Link to={link}>Check it out</Link>
                     </Button>
                 </CardContent>
             </Card>
